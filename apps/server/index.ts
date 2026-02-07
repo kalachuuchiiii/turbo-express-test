ff056059917a00d32c773678a9bab72edf11779d
kalachuuchiiii <parissrowlet@gmail.com>
2026-02-07 22:41:46 +0800

    add / route

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -1,12 +1,19 @@
 
-import express, { type Express } from 'express';
+import express,  { type Express, type Request, type Response } from 'express';
 import { USERNAME_MIN } from '@repo/constants';
 
 const app: any = express()
  
 app.use(express.json());
 
+
+app.get('/', async(req: Request , res: Response) => {
+    return res.json(`Hello ${USERNAME_MIN}`);
+})
 const PORT = 5000;
+
+
+
 app.listen(PORT, () => {
     console.log(USERNAME_MIN)
     console.log(`Listening at port ${PORT}`);
